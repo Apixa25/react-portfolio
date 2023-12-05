cfc380c2203c4fa4e5a60401e830d781b2716c1a
stevensillsii <stevensills2@gmail.com>
2023-12-04 20:35:24 -0800

    There are now 6 projects in the Portfolio section

--- src/components/Portfolio/Portfolio-projects.jsx
+++ src/components/Portfolio/Portfolio-projects.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import workRemote from "../../assets/project-images/work-remote.jpg";
 import qif from "../../assets/project-images/Qif.png";
 import textEditor from "../../assets/project-images/text-editor-pwa.gif";
+import techBlog from "../../assets/project-images/tech-blog.jpg";
+import employeeTracker from "../../assets/project-images/employee-tracker.png";
+import noteTaker from "../../assets/project-images/note-taker.gif";
 
 function Portfolio() {
   return (
@@ -46,7 +49,7 @@ function Portfolio() {
         <div className="job">
           <div className="job-info">
             <div className="job-img">
-              <a href="https://text-editor-pwa916-e253ae17fce3.herokuapp.com/">
+              <a href="https://apixa25.github.io/Qif-Generator/">
                 {" "}
                 <img
                   src={qif}
@@ -103,6 +106,92 @@ function Portfolio() {
                 utilizes this and even has an “install” feature so that the user
                 can install the Text Editor directly to their machine and be
                 able to use it completely offline.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="https://nerd-nook-news-tech-blog-cbb4ae9fd90f.herokuapp.com/login">
+                {" "}
+                <img
+                  src={techBlog}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt="Tech Blog"
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Tech Blog</h4>
+              <p>
+                Writing about tech can be just as important as making it.
+                Developers spend plenty of time creating new applications and
+                debugging existing codebases, but most developers also spend at
+                least some of their time reading and writing about technical
+                concepts, recent advancements, and new technologies. A simple
+                Google search for any concept covered in this course returns
+                thousands of think pieces and tutorials from developers of all
+                skill levels!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="https://github.com/Apixa25/employee-tracker">
+                {" "}
+                <img
+                  src={employeeTracker}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt="Employee Tracker"
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Employee Tracker</h4>
+              <p>
+                I built this employee tracker so that a person could quickly and
+                easily see all the employees in a company, who they are, what
+                they do, what department they are in, and who their manager is
+                (or maybe they are the manager! It shows that also). Also with
+                this employee tracker, a user can add new employees (including
+                name, role, salary, and who they will work for). A user can also
+                add roles, and departments also.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="https://simple-note-taker707-d91939829d81.herokuapp.com/">
+                {" "}
+                <img
+                  src={noteTaker}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt="Simple And Easy Note Taker"
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Simple And Easy Note Taker</h4>
+              <p>
+                This is a simple and clean note-taking application. You run it
+                in your browser. You make and “save” notes and they are saved
+                persistently. That means if you close your browser and turn your
+                computer off the notes you made will still be there the next
+                time you open the note taker. Your notes will stay there till
+                you choose to delete them. This application uses an Express.js
+                back end. And it saves, retrieves, and deletes notes from a JSON
+                file.
               </p>
             </div>
           </div>
