6f52e094d82e1a982001727da4727e3facd9f334
stevensillsii <stevensills2@gmail.com>
2023-12-04 19:52:06 -0800

    added 3 projects so far under portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,19 +11,39 @@ function About() {
       <div className="flex-row">
         <div class="intro-info">
           <div class="intro-img">
-            <img src={profileImage} style={{ width: "100%", padding: 30, boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)" }} alt="profile" /> 
-
+            <img
+              src={profileImage}
+              style={{
+                width: "100%",
+                padding: 30,
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+              }}
+              alt="profile"
+            />
           </div>
           <p>
-          What would I like you to know about me?  If you are looking at this thinking you might want to hire me then I would like you to know how hard of a worker I am. And that I bring to the table a very diverse set of skills because of my age and broad world experience.  The greatest of which is my ability to work with people. I have an amazing contact book. Anything anyone ever did that was great they did not do alone. If you get to know me you will hear me often say “You cannot win the Super Bowl by yourself.  It takes a whole team of great players.” That said I personally have done my best work getting to stand on the shoulders of giants.  If you hire me you are bringing far more than just me onto the team. Your bringing me and a team of wonderful people I have to draw upon.
-
-If you want to know some personal stuff about me, well… I love nature. When I am not working I am hiking and biking.  I am an explorer at heart.  I very rarely take the same trail twice. Even on the old tried and true path Ill go off trail and look for areas Ive never seen before.  Theres nothing better for clearing the mind to formulate new ideas than heading out in nature.  I have been blessed to get to live in some incredibly beautiful places! 
-
-I love friendship! So if for no other reason than to make a new friend please feel free to contact me!
-
-          
+            What would I like you to know about me? If you are looking at this
+            thinking you might want to hire me then I would like you to know how
+            hard of a worker I am. And that I bring to the table a very diverse
+            set of skills because of my age and broad world experience. The
+            greatest of which is my ability to work with people. I have an
+            amazing contact book. Anything anyone ever did that was great they
+            did not do alone. If you get to know me you will hear me often say
+            “You cannot win the Super Bowl by yourself. It takes a whole team of
+            great players.” That said I personally have done my best work
+            getting to stand on the shoulders of giants. If you hire me you are
+            bringing far more than just me onto the team. Your bringing me and a
+            team of wonderful people I have to draw upon. If you want to know
+            some personal stuff about me, well… I love nature. When I am not
+            working I am hiking and biking. I am an explorer at heart. I very
+            rarely take the same trail twice. Even on the old tried and true
+            path Ill go off trail and look for areas Ive never seen before.
+            Theres nothing better for clearing the mind to formulate new ideas
+            than heading out in nature. I have been blessed to get to live in
+            some incredibly beautiful places! I love friendship! So if for no
+            other reason than to make a new friend please feel free to contact
+            me!
           </p>
-
         </div>
       </div>
     </section>
--- src/components/Portfolio/Portfolio-projects.jsx
+++ src/components/Portfolio/Portfolio-projects.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-import sphereImage from "../../assets/project-images/work-remote.jpg";
+import workRemote from "../../assets/project-images/work-remote.jpg";
+import qif from "../../assets/project-images/Qif.png";
+import textEditor from "../../assets/project-images/text-editor-pwa.gif";
 
 function Portfolio() {
   return (
@@ -16,7 +18,7 @@ function Portfolio() {
               <a href="https://work-remote-jsn-4d3b6593bab7.herokuapp.com/">
                 {" "}
                 <img
-                  src={sphereImage}
+                  src={workRemote}
                   className="my-2"
                   style={{ width: "100%" }}
                   alt="Work-Remote-Job-Board"
@@ -24,14 +26,87 @@ function Portfolio() {
               </a>
             </div>
             <div className="job-text">
-              <h4>Sorcerer's Sphere</h4>
+              <h4>Work Remote - Remote Work Job Board</h4>
               <p>
-              Step into a realm of boundless possibilities with Work Remote, your premier destination for remote freelance work. As a hub of creativity and collaboration, we bring together seasoned professionals and visionary clients, fostering an environment where skills flourish and ideas come to life. Embrace the flexibility of remote work and discover a spectrum of projects tailored to your expertise. Join our dynamic community, where innovation knows no bounds. Your next chapter in the world of remote freelancing begins here—welcome to a realm of endless potential and unparalleled opportunities!
+                Step into a realm of boundless possibilities with Work Remote,
+                your premier destination for remote freelance work. As a hub of
+                creativity and collaboration, we bring together seasoned
+                professionals and visionary clients, fostering an environment
+                where skills flourish and ideas come to life. Embrace the
+                flexibility of remote work and discover a spectrum of projects
+                tailored to your expertise. Join our dynamic community, where
+                innovation knows no bounds. Your next chapter in the world of
+                remote freelancing begins here—welcome to a realm of endless
+                potential and unparalleled opportunities!
               </p>
             </div>
           </div>
         </div>
 
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="https://text-editor-pwa916-e253ae17fce3.herokuapp.com/">
+                {" "}
+                <img
+                  src={qif}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt="Qif - Quote and Gif Generator"
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Qif - Quote and Gif Generator</h4>
+              <p>
+                Our web-based application generates a quote and a giphy image by
+                selecting the word of your choice. The user is presented with a
+                drop-down box for their word and an “Add Word” button. Upon
+                adding the word, a random quote and gif image will display based
+                off of the word selected. The word will then be saved for the
+                user to click as many times as they desire, each time refreshing
+                with a new quote and gif image. The user is also able to click
+                the “Clear Words” button to remove all their previously added
+                words. For this project we used Zenquotes.io and
+                giphy.com.framework.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="https://text-editor-pwa916-e253ae17fce3.herokuapp.com//">
+                {" "}
+                <img
+                  src={textEditor}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt="Text Editor - Progressive Web App"
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Text Editor - Progressive Web App</h4>
+              <p>
+                This project is a Text Editor with a beautiful color interface.
+                It can be used to take notes or save code snippets with or
+                without an internet connection. This project is a progressive
+                web app (PWA). A PWA is a type of web application that utilizes
+                modern web technologies to provide an app-like experience for
+                users. One of the things that make PWA’s special is that they
+                can work with varying levels of network connectivity, including
+                offline mode. They use service workers to cache important
+                resources and provide a reliable user experience even when the
+                device is offline or has a poor network connection. This App
+                utilizes this and even has an “install” feature so that the user
+                can install the Text Editor directly to their machine and be
+                able to use it completely offline.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
